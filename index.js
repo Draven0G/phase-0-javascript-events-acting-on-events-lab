38e6dc20a4bbed919f9ea0b68a613bcd6e51948f
Ramsey <browenbroz@gmail.com>
2024-04-18 14:54:33 +0300

    bwc2

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Your code here
+document.addEventListener('keydown', function(e) {
+    if (e.key === 'ArrowLeft') {
+      moveDodgerLeft();
+    } else if (e.key === 'ArrowRight') {
+      moveDodgerRight();
+    }
+  });
+  
+  function moveDodgerLeft() {
+    const dodger = document.getElementById('dodger');
+    const dodgerLeft = parseInt(dodger.style.left.replace('px', ''), 10);
+  
+    if (dodgerLeft > 0) {
+      dodger.style.left = `${dodgerLeft - 5}px`;
+    }
+  }
+  
+  function moveDodgerRight() {
+    const dodger = document.getElementById('dodger');
+    const dodgerLeft = parseInt(dodger.style.left.replace('px', ''), 10);
+  
+    if (dodgerLeft < 360) {
+      dodger.style.left = `${dodgerLeft + 5}px`;
+    }
+  }
+  
